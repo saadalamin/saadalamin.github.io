d1f91c59c29cab80943130059cf4e633ebe9ee55
mosefatullah <mosefatullah@gmail.com>
2024-10-07 11:55:19 +0600

    .

--- src/pages/schemas/websiteSchema.js
+++ src/pages/schemas/websiteSchema.js
@@ -2,23 +2,6 @@ const websiteSchema = JSON.stringify({
  "@context": "https://schema.org",
  "@type": "WebSite",
  name: "Saad Al Amin",
- url: "https://saadalamin.com",
- description:
-  "Saad Al Amin is a Bangladeshi artist, Film Maker and Graphics Designer born 3rd august in Dhaka, Bangladesh. He was raised and educated in Dhaka, Bangladesh. He started his primary education at the age of 6. His father&apos;s name is Ruhul Amin. He is a businessman and religious leader before 2010. Furthermore, he is now working with his own Company. He is...",
- image: {
-  "@type": "ImageObject",
-  url: "https://saadalamin.com/images/persons/saad2.jpg",
-  width: 800,
-  height: 800,
- },
- publisher: {
-  "@type": "WebSite",
-  name: "Saad Al Amin",
-  logo: {
-   "@type": "ImageObject",
-   url: "https://saadalamin.com/images/persons/saad2.jpg",
-  },
- },
  potentialAction: {
   "@type": "SearchAction",
   target: {
