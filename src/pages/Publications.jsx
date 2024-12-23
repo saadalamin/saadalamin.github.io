4ac9d4297d554e7df850c60f6086259f647e81e8
saadalamin <saadalamin2004@gmail.com>
2024-12-23 12:00:01 +0600

    .

--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -6,41 +6,46 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 
 export default function Publications() {
- const { id } = useParams();
- const [navHeight, setNavHeight] = useState(0);
- useEffect(() => {
-  setNavHeight(document.querySelector(".navbar").offsetHeight);
- }, []);
- if (
-  id ==
-  "Bangladeshi_July_Revolution_Visual_Representation_Impacts_A_Film_Impact_Aspect"
- ) {
-  return (
-   <>
-    {/*-- __HEAD__ --*/}
-    <Helmet>
-     <title>
-      Bangladeshi July Revolution & Visual Representation Impacts: A Film Impact
-      Aspect - Saad Al Amin
-     </title>
-    </Helmet>
+  const { id } = useParams();
+  const [navHeight, setNavHeight] = useState(0);
+  useEffect(() => {
+    setNavHeight(document.querySelector(".navbar").offsetHeight);
+  }, []);
+  if (
+    id ==
+    "Bangladeshi_July_Revolution_Visual_Representation_Impacts_A_Film_Impact_Aspect"
+  ) {
+    return (
+      <>
+        {/*-- __HEAD__ --*/}
+        <Helmet>
+          <title>
+            [Research] Bangladeshi July Revolution & Visual Representation
+            Impacts: A Film Impact Aspect
+          </title>
+          <meta
+            name="description"
+            content="Bangladeshi July Revolution & Visual Representation Impacts: A Film Impact
+      Aspect - Saad Al Amin Research Paper"
+          />
+        </Helmet>
 
-    {/*-- __NAVBAR__ --*/}
-    <Navbar />
+        {/*-- __NAVBAR__ --*/}
+        <Navbar />
 
-    {/*-- __MAIN__ --*/}
-    <main className="blog text-center p-0">
-     <iframe
-      src="/pdfs/Bangladeshi July Revolution & Visual Representation Impacts A Film Impact Aspect.pdf"
-      style={{
-       display: "block",
-       border: "none",
-       width: "100%",
-       height: "calc(100vh - " + navHeight + "px)",
-      }}
-     ></iframe>
-    </main>
-   </>
-  );
- } else return <div>Not Found!</div>;
+        {/*-- __MAIN__ --*/}
+        <main className="blog text-center p-0">
+          <iframe
+            src="/pdfs/Bangladeshi July Revolution & Visual Representation Impacts A Film Impact Aspect.pdf"
+            style={{
+              display: "block",
+              border: "none",
+              width: "100%",
+              height: "calc(100vh - " + navHeight + "px)",
+            }}
+          ></iframe>
+        </main>
+      </>
+    );
+  } else return <div>Not Found!</div>;
 }
